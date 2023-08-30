d0bca1dc02ee918a7f4bc558f4123d202c957a63
“hariram” <hariram@crayond.co>
2023-08-30 17:17:43 +0530

    ios build shared

--- ios/com.app.pa-inspector/AppDelegate.mm
+++ ios/com.app.pa-inspector/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"app.pa_inspector";
+  self.moduleName = @"com.app.pa-inspector";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/com.app.pa-inspectorTests/com.app.pa-inspectorTests.m
+++ ios/com.app.pa-inspectorTests/com.app.pa-inspectorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface app.pa_inspectorTests : XCTestCase
+@interface com.app.pa-inspectorTests : XCTestCase
 
 @end
 
-@implementation app.pa_inspectorTests
+@implementation com.app.pa-inspectorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
